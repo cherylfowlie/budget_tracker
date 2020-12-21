87b0d35f53d75e7bcb5a823fbbff72edee5c6b9e
Cheryl <cheryljfowlie@me.com>
2020-12-21 19:19:53 +1100

    Add files to service worker

---
+++ public/serviceworker.js
@@ -0,0 +1,11 @@
+const FILES_TO_CACHE = [
+    '/',
+    'index.html',
+    'db.js',
+    'index.js',
+    'manifest.webmanifest',
+    'styles.css',
+    'icons/icon-192x192.png',
+    'icons/icon-512x512.png'
+];
+
